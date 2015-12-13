05b40de9ad6f9f090a64379d904d106e32f93694
T.J. Mazeika <t@mazeika.me>
2015-12-13 13:35:30 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget https://github.com/bionicrm/transhift/releases/download/v0.1.0-alpha/transhift-0.1.0.tar.gz
+tar -xf transhift-0.1.0.tar.gz
+rm transhift-0.1.0.tar.gz
+sudo mv transhift /usr/bin
