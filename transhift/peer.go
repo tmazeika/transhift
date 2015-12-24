58df9168fe0af5d04b27398108618495d96c8c43
T.J. Mazeika <t@mazeika.me>
2015-12-23 22:28:33 -0500

    Add comments and naming

--- transhift/peer.go
+++ transhift/peer.go
@@ -8,7 +8,6 @@ import (
     "strconv"
     "os"
     "encoding/binary"
-    "bytes"
 )
 
 // protocol errors
@@ -21,20 +20,12 @@ func portStr(port uint16) string {
     return strconv.Itoa(int(port))
 }
 
-func makePeerReadChannel(reader *bufio.Reader) (ch chan []byte) {
-    ch = make(chan []byte)
+func min(x, y uint64) uint64 {
+    if x < y {
+        return x
+    }
 
-    go func() {
-        for {
-            data, err := reader.ReadBytes('\n')
-
-            if err != nil {
-                fmt.Fprintln(os.Stderr, "Error reading line: ", err)
-            }
-
-            ch <- data
-        }
-    }()
+    return y
 }
 
 // ************************************************************************** //
@@ -146,20 +137,28 @@ func (d *UploadPeer) ReceiveFileChunks(chunkSize uint64) chan []byte {
     var totalRead uint64
 
     go func() {
+        // for as long as the total amount of bytes read is less than the file
+        // size...
         for totalRead < d.fileSize {
-            dataBuff := make([]byte, chunkSize)
+            // this is the chunk size we'll actually be using; use either the
+            // given chunk size or the number of bytes left until the end of the
+            // file, whichever is most restrictive (smaller)
+            adjustedChunkSize := min(d.fileSize - totalRead, chunkSize)
+
+            // make a new buffer to hold the bytes for this chunk
+            dataBuff := make([]byte, adjustedChunkSize)
 
             var chunkRead uint64
 
-            for chunkRead < chunkSize {
-                subDataRead, err := d.reader.Read(dataBuff[chunkRead:])
+            for chunkRead < adjustedChunkSize {
+                dataRead, err := d.reader.Read(dataBuff[chunkRead:])
 
                 if err != nil {
                     fmt.Fprintln(os.Stderr, "Error reading line: ", err)
                     return
                 }
 
-                chunkRead += subDataRead
+                chunkRead += dataRead
             }
         }
     }()
