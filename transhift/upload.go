b1ab63f1a8b589ed9d7fd5b1270da23798016c0c
T.J. Mazeika <t@mazeika.me>
2015-12-24 21:21:26 -0500

    Add upload args

--- transhift/upload.go
+++ transhift/upload.go
@@ -1,7 +1,29 @@
 package transhift
 
-import "github.com/codegangsta/cli"
+import (
+    "github.com/codegangsta/cli"
+    "path/filepath"
+)
+
+type UploadArgs struct {
+    peerHost string
+    password string
+    filePath string
+}
+
+func (a *UploadArgs) PasswordHash() []byte {
+    return calculateStringHash(a.password)
+}
+
+func (a *UploadArgs) AbsFilePath() string {
+    filePath, _ := filepath.Abs(a.filePath)
+    return filePath
+}
 
 func Upload(c *cli.Context) {
-
+    args := UploadArgs{
+        peerHost: c.Args()[0],
+        password: c.Args()[1],
+        filePath: c.Args()[2],
+    }
 }
